bb7655542b32e1c6309445f35be0778914f81fac
harish kumar <pharishkumar1306@gmail.com>
2022-02-18 18:30:22 +0530

    Initial Copy

---
+++ Front End/src/App.js
@@ -0,0 +1,13 @@
+import { Button } from '@chakra-ui/react';
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Hey Mate!</h1>
+      <Button colorScheme='teal'>Button</Button>
+    </div>
+  );
+}
+
+export default App;
